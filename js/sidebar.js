0340809d33e5dd1a0323e3cbc00e9341ae8128f8
Angel De Moya <demoyaa@gmail.com>
2022-08-04 08:17:22 -0500

    Correcciones varias front-end Home en 'angel'

--- js/sidebar.js
+++ js/sidebar.js
@@ -24,58 +24,90 @@
 // list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
 function openNav() {
-  document.getElementById("mySidebar").style.width = "16rem";
-  document.getElementById("main").style.marginLeft = "16rem";
+  document.getElementById("mySidebar").style.width = "14rem";
+  document.getElementById("mainContent").style.marginLeft = "14rem";
 }
 
-function openOrClose(x){
-  if (x==0){
+function openOrClose(x) {
+  if (x == 0) {
     openNav();
-    openbtn.value=1;
-  }else{
+    openbtn.value = 1;
+  } else {
     closeNav();
-    openbtn.value=0;
+    openbtn.value = 0;
   }
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "5rem";
-  document.getElementById("main").style.marginLeft = "5rem";
+  document.getElementById("mainContent").style.marginLeft = "5rem";
 }
-
-var modal = document.getElementById("modal-id01");
+// TODO FIX HERE
+// var modal = document.getElementById("modal-id01");
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+var modal = document.querySelector(".modal");
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
-var modal = document.getElementById("modal-id02");
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 
-
+// var modal = document.getElementById("modal-id02");
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
 
 const openEls = document.querySelectorAll("[data-open]");
 // const closeEls = document.querySelectorAll("[data-close]");
 const isVisible = "is-visible";
 
 for (const el of openEls) {
-  el.addEventListener("click", function() {
+  el.addEventListener("click", function () {
     const modalId = this.dataset.open;
     document.getElementById(modalId).classList.add(isVisible);
   });
 }
 
+// FUNCION CERRAR MODAL
+function closeModal() {
+  document.querySelector(".modal").style.display = "none";
+}
+
+// CERRAR EN ESCAPE
+document.addEventListener("keyup", (e) => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    closeModal();
+  }
+});
+
+// document.addEventListener(
+//   "click",
+//   function (event) {
+//     // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+//     if (
+//       event.target.matches(".modal-cancelbtn") ||
+//       !event.target.closest(".modal")
+//     ) {
+//       closeModal();
+//     }
+//   },
+//   false
+// );
+
 // for (const el of closeEls) {
-//   el.addEventListener("click", function() {
+//   el.addEventListener("click", function () {
 //     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
 //   });
 // }
 
-// document.addEventListener("click", e => {
+// document.addEventListener("click", (e) => {
 //   if (e.target == document.querySelector(".modal.is-visible")) {
 //     document.querySelector(".modal.is-visible").classList.remove(isVisible);
 //   }
