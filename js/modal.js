421119f81ed03934d408788500a86165564c16ca
Angel De Moya <demoyaa@gmail.com>
2022-08-05 14:35:11 -0500

    commit on 'angel'

---
+++ js/modal.js
@@ -0,0 +1,95 @@
+// TODO FIX HERE
+// var modal = document.getElementById("modal-id01");
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+
+// var modal = document.getElementById("modal-id02");
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+
+// FIXME
+const openEls = document.querySelectorAll("[data-open]");
+// const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+// FIXME END
+
+// LEER MODAL BOX
+var modal = document.querySelector(".modal");
+
+// LEER BOTON MODAL
+var btn = document.getElementsByClassName(".modal-btn");
+
+// ABRIR MODAL BOX
+function openModal() {
+  modal.style.display = "block";
+}
+// FUNCION CERRAR MODAL
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// CERRAR EN ESCAPE
+document.addEventListener("keyup", (e) => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    closeModal();
+  }
+});
+
+// // ABRIR ON CLICK A BOTON
+btn.onclick = function () {
+  openModal();
+};
+
+// CERRAR MODAL SI CLICK AFUERA
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+};
+
+// document.addEventListener(
+//   "click",
+//   function (event) {
+//     // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+//     if (
+//       event.target.matches(".modal-cancelbtn") ||
+//       !event.target.closest(".modal")
+//     ) {
+//       closeModal();
+//     }
+//   },
+//   false
+// );
+
+// for (const el of closeEls) {
+//   el.addEventListener("click", function () {
+//     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+//   });
+// }
+
+// document.addEventListener("click", (e) => {
+//   if (e.target == document.querySelector(".modal.is-visible")) {
+//     document.querySelector(".modal.is-visible").classList.remove(isVisible);
+//   }
+// });
+
+// document.addEventListener("keyup", e => {
+//   // if we press the ESC
+//   if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+//     document.querySelector(".modal.is-visible").classList.remove(isVisible);
+//   }
+// });
