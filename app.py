4c5cfdbf713ca9cff8a1db50e35ddfec65262c26
Angel De Moya <demoyaa@gmail.com>
2022-08-05 23:05:06 -0500

    commit mudanza a flask del html a 'back-end-flask'

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/test/')
+def hello():
+    return "Hello, Flask"
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/index/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
