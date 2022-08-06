31595f66e5b51f51d802b9c0b5a59e16f9275070
Angel De Moya <demoyaa@gmail.com>
2022-08-06 01:56:15 -0500

    commit para dejar sin trabajo a Javier en 'front-end'

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+
 app = Flask(__name__, static_url_path='/static')
 
 
@@ -8,39 +10,59 @@ def hello():
 
 
 @app.route('/')
-def home():
+def base():
     return render_template("base.html")
 
 
-@app.route('/index/')
-def index():
+@app.route('/home/')
+def home():
     return render_template("index.html")
 
 
-@app.route('/usuarios/')
-def users():
+@app.route('/user/')
+def user():
     return render_template("users.html")
 
 
-@app.route('/admins/')
+@app.route('/admin/')
 def admins_home():
     return render_template("homeadmin.html")
 
 
-@app.route('/admins-usuarios/')
-def admins_users():
+@app.route('/admin-users/')
+def admin_users():
     return render_template("gestionUsuarios.html")
 
 
-@app.route('/admins-admins/')
-def admins_admins():
+@app.route('/admin-admins/')
+def admin_admins():
     return render_template("gestionAdministradores.html")
 
 
-@app.route('/contacto/')
+@app.route('/contact/')
 def contact():
     return render_template("contacto.html")
 
 
+@app.route('/about/')
+def about():
+    return render_template("nosotros.html")
+
+
+@app.route('/booking/')
+def booking():
+    return render_template("reservas.html")
+
+
+@app.route('/rooms/')
+def rooms():
+    return render_template("habitaciones.html")
+
+
+@app.route('/register/')
+def register():
+    return render_template("registro.html")
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(port=4995)
