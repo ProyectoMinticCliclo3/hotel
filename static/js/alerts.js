fa7e8186a63575eb60346c798c90d8ac6489225e
Angel De Moya <demoyaa@gmail.com>
2022-08-12 12:31:36 -0500

    commit adelantos registro html y css on 'angel'

---
+++ static/js/alerts.js
@@ -0,0 +1,9 @@
+function alerta() {
+  alert("¿Está seguro que desea eliminar?");
+}
+
+function continuar() {
+  alert(
+    "¿Está seguro que los \ndatos ingresados son correctos?\n ¿Desea continuar?"
+  );
+}
